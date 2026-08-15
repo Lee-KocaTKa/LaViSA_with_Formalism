ccfed38d8ac6d685027b238458feec34c50ad44d
Lee-KocatKa <leesheenon@gmail.com>
2026-08-15 01:22:36 +0000

    loader

--- src/paths.py
+++ src/paths.py
@@ -5,10 +5,18 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]  # Resolve : pathabsolutisati
 
 DATA_DIR = PROJECT_ROOT / "data" 
 LAVISA_DIR = DATA_DIR / "lavisa" 
-ORIG_DIR = LAVISA_DIR / "original" 
-AUGMENTED_DIR = LAVISA_DIR / "augmented"
-REVISED_DIR = LAVISA_DIR / "revised"
 
+ORIG_DIR = LAVISA_DIR / "original" 
+ORIG_JSONS = ORIG_DIR / "jsons" 
+ORIG_IMAGES = ORIG_DIR / "images"
+
+AUGMENTED_DIR = LAVISA_DIR / "augmented"
+AUGMENTED_JSONS = AUGMENTED_DIR / "jsons"
+AUGMENTED_IMAGES = AUGMENTED_DIR / "images"
+
+REVISED_DIR = LAVISA_DIR / "revised"
+REVISED_JSONS = REVISED_DIR / "jsons"
+REVISED_IMAGES = REVISED_DIR / "images"
 
 OUTPUTS_DIR = PROJECT_ROOT / "outputs" 
 CONFIGS_DIR = PROJECT_ROOT / "configs" 
